c3c037e4a016d8e69d0f2de9a8e909fc5b15acb8
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2022-05-11 02:38:19 -0500

    Limpieza de código de funciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,67 @@
 function myOnLoad() {
-    funs.setOcupaciones();
+    setOcupaciones();
 }
 
-const funs = {
-    setOcupaciones: () => {
-        let ocupations = ["Administrador", "Acariciador de perritus uwu",
-            "Ama de Casa", "Científico", "Carpintero", "Docente", "Dentista",
-            "Electricista", "Estudiante", "Ganadero", "Historiador", "Joyero",
-            "Jubilado", "Mecánico", "Maquinista", "Nutricionista", "Obrero",
-            "Operador", "Panadero", "Paramédico", "Quiropráctico", "Repartidor",
-            "Relojero", "Secretaria", "Sociólogo", "Técnico", "Torero",
-            "Vendedor Ambulante", "Veterinario", "Zapatero", "Otro"];
+function setOcupaciones() {
+    let ocupations = ["Administrador", "Acariciador de perritus uwu",
+        "Ama de Casa", "Científico", "Carpintero", "Docente", "Dentista",
+        "Electricista", "Estudiante", "Ganadero", "Historiador", "Joyero",
+        "Jubilado", "Mecánico", "Maquinista", "Nutricionista", "Obrero",
+        "Operador", "Panadero", "Paramédico", "Quiropráctico", "Repartidor",
+        "Relojero", "Secretaria", "Sociólogo", "Técnico", "Torero",
+        "Vendedor Ambulante", "Veterinario", "Zapatero", "Otro"];
 
-        let selectElement = document.getElementById("ocupation");
+    let selectElement = document.getElementById("ocupation");
 
-        ocupations.forEach((ocupation) => {
-            let option = document.createElement("option")
-            option.innerHTML = ocupation;
-            selectElement.appendChild(option);
-        })
-    },
+    ocupations.forEach((ocupation) => {
+        let option = document.createElement("option")
+        option.innerHTML = ocupation;
+        selectElement.appendChild(option);
+    })
+}
 
-    getFormData: () => {
-        let form = document.getElementById('form');
-        let formData = new FormData(form);
-        let formDataObj = {};
+function getFormData() {
+    let form = document.getElementById('form');
+    let formData = new FormData(form);
+    let formDataObj = {};
 
-        formData.forEach((value, key) => {
-            if(key != 'language') {
-                formDataObj[key] = value;
-            }
-        });
+    formData.forEach((value, key) => {
+        if (key != 'language') {
+            formDataObj[key] = value;
+        }
+    });
 
-        formDataObj['language'] = formData.getAll('language');
+    formDataObj['language'] = formData.getAll('language');
 
-        console.log(formDataObj);
-        return formDataObj;
-    },
+    console.log(formDataObj);
+    return formDataObj;
+}
 
-    openPopUp: () => {
-        let overlay = document.getElementById('overlay'),
-            popup = document.getElementById('popup'),
-            cerrar = document.getElementById('btn-cerrar-popup');
-        let data = funs.getFormData();
+function openPopUp() {
+    let overlay = document.getElementById('overlay'),
+        popup = document.getElementById('popup'),
+        cerrar = document.getElementById('btn-cerrar-popup');
+    let data = getFormData();
 
-        overlay.classList.add('active');
-        popup.classList.add('active');
+    overlay.classList.add('active');
+    popup.classList.add('active');
 
-        funs.setPopUp(data);
+    setPopUp(data);
 
-        cerrar.addEventListener('click', () => {
-            overlay.classList.remove('active');
-            popup.classList.remove('active');
-        })
-    },
+    cerrar.addEventListener('click', () => {
+        overlay.classList.remove('active');
+        popup.classList.remove('active');
+    })
+}
 
-    setPopUp: (formDataObjt) => {
-        let saludo, mensaje, result;
-        let name = formDataObjt['name'],
-            language = formDataObjt['language'];
+function setPopUp(formDataObjt) {
+    let saludo, mensaje, result;
+    let name = formDataObjt['name'],
+        language = formDataObjt['language'];
 
-        saludo = "Hola " + name;
-        mensaje = (language != []) ? language.join(', ') : "";
-        result = (mensaje != "") ? saludo + "<br><br>" + "Sé buen chico y sigue programando en<br>" + mensaje : saludo;
+    saludo = "Hola " + name;
+    mensaje = (language != []) ? language.join(', ') : "";
+    result = (mensaje != "") ? saludo + "<br><br>" + "Sé buen chico y sigue programando en<br>" + mensaje : saludo;
 
-        document.getElementById('result').innerHTML = result;
-    }
+    document.getElementById('result').innerHTML = result;
 }
